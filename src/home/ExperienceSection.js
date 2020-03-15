2474b2e979448138fa123f7023a4216e1a17ab46
Gabriel Hoff <gabrielhof@gmail.com>
2020-03-15 12:03:46 -0300

    Add some transitions when the job header is sticked

--- src/home/ExperienceSection.js
+++ src/home/ExperienceSection.js
@@ -6,7 +6,7 @@ export default class ExperienceSection extends React.PureComponent {
 
   render() {
     return (
-      <div className="experience-section">
+      <section className="experience-section">
         <h3 className="heading">Work Experience</h3>
 
         <div className="timeline">
@@ -14,7 +14,7 @@ export default class ExperienceSection extends React.PureComponent {
             <Job
               title="Full-Stack Engineer"
               companyName="Z3 Works"
-              companyWebsite="http://www.z3works.com.br"
+              companyWebsite="http://www.z3works.com/"
               period="Sep 2018 - Now"
               experience={[
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
@@ -26,9 +26,22 @@ export default class ExperienceSection extends React.PureComponent {
 
             <Job
               title="Full-Stack Engineer"
-              companyName="Z3 Works"
-              companyWebsite="http://www.z3works.com.br"
-              period="Sep 2018 - Now"
+              companyName="Igen"
+              companyWebsite="http://redcap.afip.com.br/conteudo_igen.asp"
+              period="Sep 2015 - Now"
+              experience={[
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+                'Purus gravida quis blandit turpis cursus in hac habitasse platea. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum a.',
+                'Nibh sit amet commodo nulla facilisi nullam vehicula ipsum a.',
+                'Sit amet risus nullam eget felis eget nunc lobortis. Tellus orci ac auctor augue mauris.'
+              ]}
+            />
+
+            <Job
+              title="Full-Stack Developer"
+              companyName="I360 Technology"
+              companyWebsite="http://i360tecnologia.com.br"
+              period="Sep 2015 - Now"
               experience={[
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
                 'Purus gravida quis blandit turpis cursus in hac habitasse platea. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum a.',
@@ -38,7 +51,7 @@ export default class ExperienceSection extends React.PureComponent {
             />
           </div>
         </div>
-      </div>
+      </section>
     );
   }
 
--- src/home/experience/Job.js
+++ src/home/experience/Job.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import ExternalLink from 'src/components/ExternalLink';
 
 export default class Job extends React.PureComponent {
@@ -12,6 +13,55 @@ export default class Job extends React.PureComponent {
     experience: PropTypes.arrayOf(PropTypes.string)
   }
 
+  jobItemRef = new React.createRef()
+  jobHeaderRef = new React.createRef()
+  headerObserver = null;
+
+  state = {
+    headerSticked: false
+  }
+
+  componentDidMount() {
+    this.headerObserver = new IntersectionObserver(entries => {
+      const [headerEntry] = entries;
+      this.handleHeaderPositionChanged(headerEntry);
+    }, {
+      threshold: [1]
+    });
+
+    if (!this.jobHeaderRef.current) {
+      return;
+    }
+
+    this.headerObserver.observe(this.jobHeaderRef.current);
+  }
+
+  componentWillUnmount() {
+    if (!this.headerObserver) {
+      return;
+    }
+
+    if (!this.jobHeaderRef.current) {
+      return;
+    }
+
+    this.headerObserver.unobserve(this.jobHeaderRef.current);
+  }
+
+  handleHeaderPositionChanged = headerEntry => {
+    const {intersectionRatio, boundingClientRect} = headerEntry;
+
+    const shouldStick = (intersectionRatio < 1 && boundingClientRect.top <= 0);
+
+    if (shouldStick === this.state.headerSticked) {
+      return;
+    }
+
+    this.setState({
+      headerSticked: shouldStick
+    });
+  }
+
   render() {
     const {
       title,
@@ -22,8 +72,17 @@ export default class Job extends React.PureComponent {
     } = this.props;
 
     return (
-      <div className="job-item">
-        <div className="job-header">
+      <div
+        ref={this.jobItemRef}
+        className="job-item"
+      >
+        <div
+          ref={this.jobHeaderRef}
+          className={classNames({
+            'job-header': true,
+            'sticky': this.state.headerSticked
+          })}
+        >
           <h5 className="period">{period}</h5>
 
           <h4 className="title-and-company">
